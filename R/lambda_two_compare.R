5813faa1dd1571c496f026ef394dfe8150bc1415
Loren Collingwood <loren.collingwood@ucr.edu>
2018-11-28 22:27:01 -0800

    Added lambda functions

---
+++ R/lambda_two_compare.R
@@ -0,0 +1,46 @@
+lambda_two_compare <- function(lmd, cnames, group_name="Latino", 
+                               cand1or2=1) {
+  
+  if (cand1or2==2){
+    
+    cnames <- rev(cnames)
+    p.cand_over_cand<-lmd[,cnames[2]]-lmd[,cnames[1]]
+    
+  }   
+  
+  n <- nrow(lmd)
+  labels <- sub('.*\\.', '', cnames)
+  
+  #what is probability that Morales is preferred to Rothman among Latinos?
+  p.cand_over_cand<-lmd[,cnames[1]]-lmd[,cnames[2]]
+  
+  # Plot Histogram & Posterior #
+  par(mfrow=c(1,2))
+  
+  # Histogram #
+  hist (lmd[,cnames[1]] - lmd[,cnames[2]], 
+        main = paste(labels[1], " - ",labels[2], sep=""),
+        xlab = "Posterior Distribution")
+  
+  # Density #
+  
+  # Need regular expression for everything to right of period to extract names #
+  dens=density(p.cand_over_cand)
+  plot(dens,main=paste("Difference in Proportion ", group_name, 
+                       "\nVote for ", labels[1], " and ", labels[2], sep=""),
+       xlab="Difference in Posterior Distribution Sampled from ")
+  points(p.cand_over_cand,rep(0,length(p.cand_over_cand)),pch=3) #plot individual posterior sampled points (no)
+  abline(v=0, col="grey", lty=2)
+  
+  # Probability 1 Candidate over Another #
+  c1g10 <- length(which(p.cand_over_cand>.10))/n # Morales over Rothman by at least 10 points
+  c1g5 <- length(which(p.cand_over_cand>.05))/n # # Morales over Rothman by at least 5 points
+  c1g0 <- length(which(p.cand_over_cand>0))/n
+  
+  med <- median(p.cand_over_cand)
+  mean <- mean(p.cand_over_cand)
+  df <- data.frame(c1g10, c1g5, c1g0, round(med,3), round(mean, 3))
+  colnames(df) <- c("Prob>10%", "Prob>5%", "Prob>0", "Dist. Median", "Dist. Mean")
+  return( df )
+  
+}
---
+++ R/md_bayes_draw_lambda.R
@@ -0,0 +1,17 @@
+md_bayes_draw_lambda <- function (dat, race_vote_split, form, ntunes = 10, totaldraws = 1e+05, 
+          seed = 12345, sample = 1e+05, thin = 100, burnin = 1e+05, 
+          ret.mcmc = TRUE) {
+  left_dat <- apply(dat[, 1:race_vote_split[1]], 1, sum)
+  right_dat <- apply(dat[, race_vote_split[2]:ncol(dat)], 1, 
+                     sum)
+  stopifnot(all.equal(left_dat, right_dat))
+  formula1 <- form
+  set.seed(seed)
+  tune.nocov <- tuneMD(formula1, data = dat, ntunes = ntunes, 
+                       totaldraws = totaldraws)
+  md.out <- ei.MD.bayes(formula1, data = dat, sample = sample, 
+                        thin = thin, burnin = burnin, ret.mcmc = ret.mcmc, tune.list = tune.nocov)
+  cat(paste("Taking first ",race_vote_split[1], " names from dat object\n", sep="" ))
+  lmd <- lambda.MD(md.out,columns=names(dat[, 1:race_vote_split[1]]))
+  return(lmd)
+}
