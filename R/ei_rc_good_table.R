cda04abcc9821d695aa0aa20e0647eceb1b81e7a
Pratik Sachdeva <pratik.sachdeva23@gmail.com>
2020-07-23 16:53:30 -0700

    include import for new in rc good table

--- R/ei_rc_good_table.R
+++ R/ei_rc_good_table.R
@@ -126,15 +126,16 @@
 #' )
 #' }
 #'
+#' @import methods new
 #' @export ei_rc_good_table
 ei_rc_good_table <-
-  function(ei, rc, good, groups, include_good = F) {
+  function(ei, rc, good, groups, include_good = FALSE) {
     candidate <- ei[, 1]
-    # Number of Racial/Ethnic Groups
+    # number of racial/ethnic groups
     num_group <- ncol(ei) - 1 #-1 because subtract candidate column
 
-    if (include_good == T) {
-      # Put Results together that then needs to get broken apart
+    if (include_good) {
+      # put results together that then needs to get broken apart
       comb_table <- cbind(ei, rc[, 2:ncol(rc)], good[, 2:ncol(good)])
 
       i <- 0
@@ -144,7 +145,7 @@ ei_rc_good_table <-
       while (i <= ncol(ei) - 2) {
         tab_select <- seq(2 + i, ncol(comb_table), num_group)
         tab_reorg <- comb_table[, tab_select]
-        tab_reorg$EI_Diff <- tab_reorg[, 2] - tab_reorg[, 1] # RxC - EI
+        tab_reorg[["EI_Diff"]] <- tab_reorg[, 2] - tab_reorg[, 1] # RxC - EI
         tab_reorg$EI_Diff[seq(2, length(tab_reorg$EI_Diff), 2)] <- NA
         i <- i + 1 # trick to get table into first list object
         tab_list[[i]] <- tab_reorg
@@ -154,7 +155,7 @@ ei_rc_good_table <-
       nam <- unlist(nam)
       colnames(tab_out)[2:ncol(tab_out)] <- nam
     } else {
-      # Put Results together that then needs to get broken apart
+      # put results together that then needs to get broken apart
       comb_table <- cbind(ei, rc[, 2:ncol(rc)])
 
       i <- 0
@@ -164,9 +165,10 @@ ei_rc_good_table <-
       while (i <= ncol(ei) - 2) {
         tab_select <- seq(2 + i, ncol(comb_table), num_group)
         tab_reorg <- comb_table[, tab_select]
-        tab_reorg$EI_Diff <- tab_reorg[, 2] - tab_reorg[, 1] # RxC - EI
+        tab_reorg[["EI_Diff"]] <- tab_reorg[, 2] - tab_reorg[, 1] # RxC - EI
         tab_reorg$EI_Diff[seq(2, length(tab_reorg$EI_Diff), 2)] <- NA
-        i <- i + 1 # trick to get table into first list object
+        # trick to get table into first list object
+        i <- i + 1
         tab_list[[i]] <- tab_reorg
         nam[[i]] <- names(tab_reorg)
       }
@@ -174,8 +176,8 @@ ei_rc_good_table <-
       nam <- unlist(nam)
       colnames(tab_out)[2:ncol(tab_out)] <- nam
     }
-    # Create Class object of "ei_compare"
-    tab_out <- new("ei_compare", data = tab_out, groups = groups)
+    # create class object of "ei_compare"
+    tab_out <- methods::new("ei_compare", data = tab_out, groups = groups)
 
     return(tab_out)
   }
