7c52a974041ceb2dde2e3ac7e3b9b609b395b3cb
Pratik Sachdeva <pratik.sachdeva23@gmail.com>
2020-07-23 12:52:55 -0700

    update variable to snake case in lambda_two_compare

--- R/lambda_two_compare.R
+++ R/lambda_two_compare.R
@@ -41,14 +41,14 @@ lambda_two_compare <- function(lmd, cnames, group_name = "Latino",
                                cand1or2 = 1) {
   if (cand1or2 == 2) {
     cnames <- rev(cnames)
-    p.cand_over_cand <- lmd[, cnames[2]] - lmd[, cnames[1]]
+    p_cand_over_cand <- lmd[, cnames[2]] - lmd[, cnames[1]]
   }
 
   n <- nrow(lmd)
   labels <- sub(".*\\.", "", cnames)
 
   # what is probability that Morales is preferred to Rothman among Latinos?
-  p.cand_over_cand <- lmd[, cnames[1]] - lmd[, cnames[2]]
+  p_cand_over_cand <- lmd[, cnames[1]] - lmd[, cnames[2]]
 
   # Plot Histogram & Posterior #
   par(mfrow = c(1, 2))
@@ -62,7 +62,7 @@ lambda_two_compare <- function(lmd, cnames, group_name = "Latino",
   # Density #
 
   # Need regular expression for everything to right of period to extract names
-  dens <- density(p.cand_over_cand)
+  dens <- density(p_cand_over_cand)
   plot(dens,
     main = paste("Difference in Proportion ", group_name,
       "\nVote for ", labels[1], " and ", labels[2],
@@ -71,18 +71,18 @@ lambda_two_compare <- function(lmd, cnames, group_name = "Latino",
     xlab = "Difference in Posterior Distribution Sampled from "
   )
   # plot individual posterior sampled points (no)
-  points(p.cand_over_cand, rep(0, length(p.cand_over_cand)), pch = 3)
+  points(p_cand_over_cand, rep(0, length(p_cand_over_cand)), pch = 3)
   abline(v = 0, col = "grey", lty = 2)
 
   # Probability 1 Candidate over Another #
   # Morales over Rothman by at least 10 points
-  c1g10 <- length(which(p.cand_over_cand > .10)) / n
+  c1g10 <- length(which(p_cand_over_cand > .10)) / n
   # Morales over Rothman by at least 5 points
-  c1g5 <- length(which(p.cand_over_cand > .05)) / n
-  c1g0 <- length(which(p.cand_over_cand > 0)) / n
+  c1g5 <- length(which(p_cand_over_cand > .05)) / n
+  c1g0 <- length(which(p_cand_over_cand > 0)) / n
 
-  med <- median(p.cand_over_cand)
-  mean <- mean(p.cand_over_cand)
+  med <- median(p_cand_over_cand)
+  mean <- mean(p_cand_over_cand)
   df <- data.frame(c1g10, c1g5, c1g0, round(med, 3), round(mean, 3))
   colnames(df) <- c(
     "Prob>10%",
