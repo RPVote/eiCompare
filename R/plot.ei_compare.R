6171513f00c48743e751e7bd786371910bb2499c
hikari-murayama <hikari_murayama@berkeley.edu>
2020-07-24 00:15:01 +0000

    update plot.ei_compare

--- R/plot.ei_compare.R
+++ R/plot.ei_compare.R
@@ -1,12 +1,12 @@
 #' Plot Method for class ei_compare
-#' 
+#'
 #' Allows quick plotting, using plot() of EI vs EI:RxC differences. Produces
 #' ggplot2 ouput, amazing.
-#' 
+#'
 #' Limited amount of plotting flexibility. If user wants more flexibility
 #' extract relevant objects from ei_rc_good_table() output and do your own
 #' plotting!
-#' 
+#'
 #' @param x Object of class ei_compare, from the ei_rc_good_table() function
 #' @param \dots Arguments passed onto plot() and par()
 #' @return ggplot2 graph output of EI and RxC differences
@@ -14,82 +14,89 @@
 #' <garcia.rios@@cornell.edu>
 #' @references eiPack, King et. al. (http://gking.harvard.edu/eiR)
 #' @examples
-#' 
-#' 
+#'
+#'
 #' # TOY DATA EXAMPLE
 #' canda <- c(.1, .09, .85, .9, .92)
-#' candb <- 1-canda
+#' candb <- 1 - canda
 #' white <- c(.8, .9, .10, .08, .11)
 #' black <- 1 - white
-#' total <- c(30,80, 70, 20, 29)
+#' total <- c(30, 80, 70, 20, 29)
 #' toy <- data.frame(canda, candb, white, black, total)
-#' 
+#'
 #' # CREATE VECTORS
 #' cands <- c("canda")
 #' race_group <- c("~ black") # only use one group for example
 #' table_names <- c("EI: PCT Black", "EI: PCT White")
-#' 
+#'
 #' # RUN ei_est_gen()
 #' # KEEP DATA TO JUST ONE ROW FOR EXAMPLE (time) ONLY!
 #' results <- ei_est_gen(cands, race_group, "total",
-#'                       data = toy[c(1,3,5),], table_names = table_names, sample=100)
-#' 
-#' # Generate formula for passage to ei.reg.bayes() function
-#' form <- formula(cbind(canda,candb) ~ cbind(black, white)) 
-#' # Run Bayesian model
-#' suppressWarnings (
-#'   ei_bayes <- ei.reg.bayes(form, data=toy, sample=100, truncate=TRUE)
+#'   data = toy[c(1, 3, 5), ], table_names = table_names, sample = 100
 #' )
-#' 
+#'
+#' # Generate formula for passage to ei.reg.bayes() function
+#' form <- formula(cbind(canda, candb) ~ cbind(black, white))
+#' # Run Bayesian model
+#' suppressWarnings(
+#'   ei_bayes <- ei.reg.bayes(form, data = toy, sample = 100, truncate = TRUE)
+#' )
+#'
 #' table_names <- c("RxC: PCT Black", "RxC: PCT White")
 #' cands <- c("canda", "candb")
-#' ei_bayes_res <- bayes_table_make(ei_bayes, cand_vector= cands, table_names = table_names)
-#' ei_bayes_res <- ei_bayes_res[c(1,2,5),]
-#' # Combine Results, results in object of class ei_compare 
-#' ei_rc_combine <- ei_rc_good_table(results, ei_bayes_res, 
-#'                                   groups= c("Black", "White")
+#' ei_bayes_res <- bayes_table_make(ei_bayes, cand_vector = cands, table_names = table_names)
+#' ei_bayes_res <- ei_bayes_res[c(1, 2, 5), ]
+#' # Combine Results, results in object of class ei_compare
+#' ei_rc_combine <- ei_rc_good_table(results, ei_bayes_res,
+#'   groups = c("Black", "White")
 #' )
 #' # Produces data and character vector, which can be sent to plot()
 #' ei_rc_combine
-#' 
+#'
 #' # PLOT EI DIFFERENCES
 #' plot(ei_rc_combine)
-#' 
 #' \donttest{
 #' # Warning: Takes a while to run
 #' # Load corona data
 #' data(corona)
 #' # Generate character vectors
-#' cands <- c("pct_husted","pct_spiegel","pct_ruth","pct_button","pct_montanez","pct_fox")
+#' cands <- c("pct_husted", "pct_spiegel", "pct_ruth", "pct_button", "pct_montanez", "pct_fox")
 #' race_group3 <- c("~ pct_hisp", "~ pct_asian", "~ pct_white")
 #' table_names <- c("EI: Pct Lat", "EI: Pct Asian", "EI: Pct White")
 #' # Run EI iterative Fitting
-#' results <- ei_est_gen(cand_vector=cands, race_group = race_group3,
-#'                       total = "totvote", data = corona, table_names = table_names)
-#' # EI: RxC model 
+#' results <- ei_est_gen(
+#'   cand_vector = cands, race_group = race_group3,
+#'   total = "totvote", data = corona, table_names = table_names
+#' )
+#' # EI: RxC model
 #' # Generate formula
-#' form <- formula(cbind(pct_husted,pct_spiegel,pct_ruth,pct_button,pct_montanez,pct_fox) 
-#' ~ cbind(pct_hisp, pct_asian, pct_white)) 
-#' suppressWarnings (
-#'   ei_bayes <- ei.reg.bayes(form, data=corona, sample=10000, truncate=TRUE)
+#' form <- formula(cbind(pct_husted, pct_spiegel, pct_ruth, pct_button, pct_montanez, pct_fox)
+#' ~ cbind(pct_hisp, pct_asian, pct_white))
+#' suppressWarnings(
+#'   ei_bayes <- ei.reg.bayes(form, data = corona, sample = 10000, truncate = TRUE)
 #' )
 #' # RxC table names
 #' table_names <- c("RxC: Pct Hisp", "RxC: Pct Asian", "RxC: Pct White")
 #' # Table Creation, using function bayes_table_make in ei_est_generalize.R file
-#' ei_bayes_res <- bayes_table_make(ei_bayes, cand_vector= cands, table_names = table_names)
-#' 
+#' ei_bayes_res <- bayes_table_make(ei_bayes, cand_vector = cands, table_names = table_names)
+#'
 #' # Goodman Regression
 #' table_names <- c("Good: Pct Lat", "Good: Pct Asian", "Good: Pct Wht")
 #' good_corona <- goodman_generalize(cands, race_group3, "totvote", corona, table_names)
-#' 
-#' # Combine Results, results in object of class ei_compare 
+#'
+#' # Combine Results, results in object of class ei_compare
 #' ei_rc_g_combine <- ei_rc_good_table(results, ei_bayes_res, good_corona,
-#'                                     groups= c("Latino", "Asian", "White")
+#'   groups = c("Latino", "Asian", "White")
 #' )
 #' # Plot the Results
 #' plot(ei_rc_g_combine)
 #' }
-#' 
+#'
+#' @importFrom photobiology na.omit
+#' @import ggplot2
+#' aes, geom_point, position_dodge, labs, geom_hline, geom_linerange, coord_flip, theme_bw, ggtitle, theme, element_text
+#'
+#'
 #' @export plot.ei_compare
 plot.ei_compare <- function(x, ...) {
 
@@ -100,10 +107,16 @@ plot.ei_compare <- function(x, ...) {
   if (!is.na(x@data[2, 4])) stop("In 'ei_rc_good_table()' function set include_good=F, I'm cereal you guys")
 
   # Collect Candidate Names for Labeling
-  ei_rc_combine_a <- na.omit(x@data)
+  ei_rc_combine_a <- photobiology::na.omit(x@data)
   cand_names_plot <- as.character(ei_rc_combine_a[-nrow(ei_rc_combine_a), 1])
   # Subset Point Estimates Just to Differences
-  ei_rc_combine_a <- ei_rc_combine_a[-nrow(ei_rc_combine_a), grep("EI_Diff", colnames(ei_rc_combine_a), fixed = T)] # Gets rid of Total row and selects only DIFF columns
+  ei_rc_combine_a <- ei_rc_combine_a[
+    -nrow(ei_rc_combine_a),
+    grep("EI_Diff",
+      colnames(ei_rc_combine_a),
+      fixed = T
+    )
+  ] # Gets rid of Total row and selects only DIFF columns
   ei_rc_combine_a <- data.frame(cand_names_plot, ei_rc_combine_a)
   # Reshape the data for ggplot()
   tidy_it <- ei_rc_combine_a %>% tidyr::gather(Group, value, -cand_names_plot)
@@ -140,29 +153,32 @@ plot.ei_compare <- function(x, ...) {
   value <- Group <- se <- se_1_2 <- NULL
   # Produce the GGPLOT
   suppressWarnings( # Warning: Ignoring unknown aesthetics: y
-    ggplot(tidy_it, aes(
+    ggplot2::ggplot(tidy_it, ggplot2::aes(
       x = cand_names_plot, y = value,
       shape = factor(Group, labels = x@groups),
       color = factor(Group, labels = x@groups)
     )) +
       # Manipulate Point spacing
-      geom_point(position = position_dodge(width = rep(.5, nplots)), size = 3) +
-      labs(color = "Group", shape = "Group") +
-      geom_hline(yintercept = 0, colour = "gray", linetype = 2, size = 1.5) +
+      ggplot2::geom_point(position = position_dodge(width = rep(.5, nplots)), size = 3) +
+      ggplot2::labs(color = "Group", shape = "Group") +
+      ggplot2::geom_hline(yintercept = 0, colour = "gray", linetype = 2, size = 1.5) +
       # Adjust Error Bars for 1 and 2 SEs
-      geom_linerange(aes(
+      ggplot2::geom_linerange(ggplot2::aes(
         x = cand_names_plot, y = value,
         ymax = value + se, ymin = value - se
       ),
-      position = position_dodge(width = rep(.5, nplots)), size = 1.5
+      position = ggplot2::position_dodge(width = rep(.5, nplots)), size = 1.5
       ) +
-      geom_linerange(aes(x = cand_names_plot, y = value, ymax = value + se_1_2, ymin = value - se_1_2),
-        position = position_dodge(width = rep(.5, nplots)), size = .8
+      ggplot2::geom_linerange(ggplot2::aes(
+        x = cand_names_plot, y = value,
+        ymax = value + se_1_2, ymin = value - se_1_2
+      ),
+      position = ggplot2::position_dodge(width = rep(.5, nplots)), size = .8
       ) +
-      coord_flip() +
-      theme_bw() +
-      ggtitle("Estimate Difference of EI and RxC Methods") +
-      theme(plot.title = element_text(size = 20, face = "bold")) +
-      labs(x = "", y = "RxC-EI Estimate")
+      ggplot2::coord_flip() +
+      ggplot2::theme_bw() +
+      ggplot2::ggtitle("Estimate Difference of EI and RxC Methods") +
+      ggplot2::theme(plot.title = element_text(size = 20, face = "bold")) +
+      ggplot2::labs(x = "", y = "RxC-EI Estimate")
   ) # Close Warning: Ignoring unknown aesthetics: y message
 } # Close Function
