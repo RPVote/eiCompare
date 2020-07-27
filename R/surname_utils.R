314e592ad454b3f5356f60bb6b5cd6ca523d97d2
Pratik Sachdeva <pratik.sachdeva23@gmail.com>
2020-07-27 09:58:14 -0700

    first commit of surname utils

---
+++ R/surname_utils.R
@@ -0,0 +1,223 @@
+#' Counts the number of words per row in the column of a dataframe.
+#'
+#' A "word" is defined as a string of alphabetical characters separated by
+#' either spaces or dashes (but not other special characters).
+#'
+#' @param voter_file The voter file, with each row consisting of a voter.
+#' @param surname_col A string denoting the surname column.
+#' @param regex A string denoting the regular expression to use for querying the
+#'   the word count.
+#' @return A vector of word counts.
+#'
+#' @export get_word_count
+#' @importFrom stringr str_count
+get_word_count <- function(voter_file,
+                           col = "last_name",
+                           regex = "[ -]+") {
+  word_count <- 1 + stringr::str_count(voter_file[[col]], regex)
+  return(word_count)
+}
+
+
+#' Gets special characters in a column of names.
+#'
+#' Returns a unique list of special characters found in a column of a dataframe.
+#' By default, these characters consist of any that are not upper- or lower-case
+#' letters. This preference can be overwritten by providing a new regular
+#' expression.
+#'
+#' @param voter_file The voter file, with each row consisting of a voter.
+#' @param surname_col A string denoting the surname column.
+#' @param regex A string denoting the regular expression to use for querying the
+#'   the special characters.
+#' @return A vector of unique special characters found in the names.
+#'
+#' @export get_unique_special_characters
+#' @importFrom stringr str_extract
+get_unique_special_characters <- function(voter_file,
+                                          surname_col = "last_name",
+                                          regex = "[^A-Za-z]") {
+  special_surnames <- stringr::str_extract(voter_file[[surname_col]], regex)
+  characters <- unique(special_surnames)
+  return(characters)
+}
+
+
+#' Gets surnames containing special characters.
+#'
+#' Returns a subsetted voter file whose rows consist of voters that have
+#' special characters in their last name.
+#'
+#' @param voter_file The voter file, with each row consisting of a voter.
+#' @param surname_col A string denoting the surname column.
+#' @param regex A string denoting the regular expression to use for querying the
+#'   the special characters.
+#' @return A dataframe of voters whose surname has special characters.
+#'
+#' @export get_special_character_surnames
+#' @importFrom stringr str_extract
+get_special_character_surnames <- function(voter_file,
+                                           surname_col = "last_name",
+                                           regex = "[^A-Za-z]") {
+  special_characters <- stringr::str_detect(voter_file[[surname_col]], regex)
+  return(voter_file[special_characters, ])
+}
+
+
+#' Strips special characters from a voter file.
+#'
+#' Given a voter file and a column, returns a voter file with special characters
+#' stripped stripped from that column.
+#'
+#' @param voter_file The voter file, with each row consisting of a voter.
+#' @param surname_col A string denoting the surname column.
+#' @param regex A string denoting the regular expression to use for denoting the
+#'   the special characters.
+#' @return A dataframe of voters whose surname column is stripped of special
+#'  characters.
+#'
+#' @export strip_special_characters
+#' @importFrom stringr str_replace
+strip_special_characters <- function(voter_file,
+                                     surname_col = "last_name",
+                                     regex = "[^A-Za-z]",
+                                     replace = " ") {
+  # Replace special characters with empty spaces
+  voter_file[[surname_col]] <- stringr::str_replace(
+    voter_file[[surname_col]],
+    regex,
+    replace
+  )
+  return(voter_file)
+}
+
+
+#' Gets double-barreled surnames from a voter file.
+#'
+#' A double-barreled surname is one containing a dash or a space. This function
+#' finds all double-barreled surnames in a voter file.
+#'
+#' @param voter_file The voter file, with each row consisting of a voter.
+#' @param surname_col A string denoting the surname column.
+#' @param regex A string denoting the regular expression to use for denoting the
+#'   the special characters.
+#' @return A dataframe of voters whose surnames are double-barreled.
+#'
+#' @export get_double_barreled_surnames
+#' @importFrom stringr str_detect
+get_double_barreled_surnames <- function(voter_file,
+                                         surname_col = "last_name",
+                                         regex = "[ -]+") {
+  doubled_barreled <- stringr::str_detect(voter_file[[surname_col]], regex)
+  return(voter_file[doubled_barreled, ])
+}
+
+
+#' Determines which surnames match to the Census list.
+#'
+#' @param voter_file The voter file, with each row consisting of a voter.
+#' @param surname_col A string denoting the surname column.
+#' @param strip_special Whether to strip special characters before matching in
+#'  the surname database.
+#' @return A vector of logicals denoting a match or not.
+#'
+#' @export surname_match
+surname_match <- function(voter_file,
+                          surname_col = "last_name",
+                          strip_special = FALSE) {
+  if (strip_special) {
+    voter_file <- eiCompare::strip_special_characters(
+      voter_file = voter_file,
+      surname_col = surname_col,
+      replace = ""
+    )
+  }
+  # Determine if there's a surname match
+  surname_match <- voter_file[[surname_col]] %in% wru::surnames2010$surname
+  return(surname_match)
+}
+
+
+#' Briefly summarizes the surnames in a voter file.
+#'
+#' @param voter_file The voter file, with each row consisting of a voter.
+#' @param surname_col A string denoting the surname column.
+#'
+#' @export surname_summary
+surname_summary <- function(voter_file, surname_col) {
+  # Print number of voters
+  n_voters <- nrow(voter_file)
+  message(paste("Voter file has", n_voters, "voters."))
+
+  # Print number of voters with no last name
+  n_nan_surnames <- sum(is.na(voter_file[[surname_col]]))
+  message(paste("Voter files has", n_nan_surnames, "voter(s) with no surname."))
+
+  # Print number of voters with special characters
+  special_character_surnames <- eiCompare::get_special_character_surnames(
+    voter_file = voter_file,
+    surname_col = surname_col
+  )
+  n_special_character_surnames <- nrow(special_character_surnames)
+  message(paste(
+    "Voter file has", n_special_character_surnames,
+    "voters containing special characters."
+  ))
+
+  # Print number of matches by default
+  n_organic_matches <- sum(eiCompare::surname_match(
+    voter_file = voter_file,
+    surname_col = surname_col,
+    strip_special = FALSE
+  ))
+  message(paste(
+    "Voter file has", n_organic_matches,
+    "voters with surnames matching the database by default."
+  ))
+
+  # Print number of matches after stripping special characters
+  n_stripped_matches <- sum(eiCompare::surname_match(
+    voter_file = voter_file,
+    surname_col = surname_col,
+    strip_special = TRUE
+  ))
+  message(paste(
+    "Voter file has", n_stripped_matches,
+    "voters with surnames matching the database after removing",
+    "special characters."
+  ))
+}
+
+
+
+#' Predicts, for one row in a voter file, the probability of a voter having a
+#' certain race by averaging over each "barrel" of the surname.
+#'
+#' @param voter_file The voter file, with each row consisting of a voter.
+#' @param surname_col A string denoting the surname column.
+#' @param surname_only Whether to obtain probabilities for surnames only.
+#' @param pattern What pattern to split surnames on. By default, surnames are
+#'  split on a space(s), which assumes hyphens have already been removed.
+#' @return A vector of probabilities for each surname.
+#'
+#' @export predict_race_double_barreled
+predict_race_double_barreled <- function(df,
+                                         surname_col = "last_name",
+                                         surname_only = TRUE,
+                                         pattern = "[ ]+") {
+  surnames <- stringr::str_split(df[[surname_col]], pattern = pattern)[[1]]
+  surnames <- data.frame(surname = surnames)
+
+  if (surname_only) {
+    probabilities <- suppressWarnings(
+      wru::merge_surnames(
+        voter.file = surnames,
+        surname.year = 2010,
+        clean.surname = FALSE,
+        impute.missing = TRUE
+      )
+    )
+    probabilities <- as.numeric(colMeans(probabilities[, c(-1, -2)]))
+  }
+  return(probabilities)
+}
