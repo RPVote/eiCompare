cad7e3dd60026ee9546ec3a00b2adf34af3672a2
aridf <adecterfrain@gmail.com>
2020-07-28 13:27:55 +0000

    style

--- R/ei_preprocessing.R
+++ R/ei_preprocessing.R
@@ -1,42 +1,42 @@
 #' Internal function that checks for adequate closeness between sums
 #' of race/candidate columns and provided vote totals.
-#' 
+#'
 #' This function checks conditions inputted to clean_race by the user
-#' 
-#' When this function returns 0, votes do not sum close enough 
-#' to the provided vote totals. 
-#' 
-#' When this function returns 1, votes sum  marginally away 
+#'
+#' When this function returns 0, votes do not sum close enough
+#' to the provided vote totals.
+#'
+#' When this function returns 1, votes sum  marginally away
 #' from the provided totals. Restandardization is appropriate
-#'  
+#'
 #' When this function returns 2, votes sum exactly to vote
-#' totals 
-#' 
+#' totals
+#'
 #' @author Ari Decter-Frain
-#' 
+#'
 #' @param vote_sums A numeric vector containing the sums of votes by each race/ethnicity
 #' @param provided_totals A numeric vector containing the totals provided to the clean_race function
 #' @param max_dev A flaoting point object passed from clean_race
 #' @param avg_dev A floating point object passed from clean_race
-#' 
+#'
 #' @keywords internal
-#' 
+#'
 #' @return A list containing two objects, 'closeness' and 'deviates'. See details for more information
 check_diffs <- function(vote_sums, provided_totals, max_dev, avg_dev) {
-  
+
   # check max_dev, avg_dev validity
-  if((max_dev < 0) | (avg_dev < 0)) {
+  if ((max_dev < 0) | (avg_dev < 0)) {
     stop(
       "max_dev and avg_dev must be greater than 0"
     )
   }
-  
+
   # get deviation info
   diff_prps <- abs(vote_sums - provided_totals) / provided_totals
   deviates <- diff_prps != 0
   max <- max(diff_prps)
   avg <- mean(diff_prps)
-  
+
   # check conditions
   output <- list(closeness = 2, deviates = deviates)
   if (all(diff_prps == 0)) {
@@ -51,32 +51,34 @@ check_diffs <- function(vote_sums, provided_totals, max_dev, avg_dev) {
 }
 
 #' Computes proportions for returning by clean_race and clean_cand
-#' 
+#'
 #' @author Ari Decter-Frain
-#' 
+#'
 #' @param votes A dataframe of raw votes by race or candidates
-#' 
+#'
 #' @keywords internal
-#' 
+#'
 #' @return A dataframe of standardized proportions whose columns sum rowwise to 1
 standardize_votes <- function(votes) {
-  prps <- votes / rowSums(votes)
+  totals <- rowSums(votes)
+  prps <- votes / totals
   names(prps) <- paste(names(prps), "p", sep = "_")
-  return(prps)
+  output <- cbind(prps, data.frame("total" = totals))
+  return(output)
 }
 
-#' Converts raw vote totals from different voter groups / 
-#' candidates across precincts into proportions, checking 
-#' for problematic differences between known vote totals 
+#' Converts raw vote totals from different voter groups /
+#' candidates across precincts into proportions, checking
+#' for problematic differences between known vote totals
 #' and sums across race/ethnicities.
-#' 
-#' If turnout columns sum row-wise to equal vote_totals, they are 
-#' returned as proportions. 
-#'  
-#' If turnout columns sum row-wise to sufficiently close to 
+#'
+#' If turnout columns sum row-wise to equal vote_totals, they are
+#' returned as proportions.
+#'
+#' If turnout columns sum row-wise to sufficiently close to
 #' vote_totals, they are returned as proportions of the sums.
-#'  
-#' If turnout columns sum row-wise exceedingly far from 
+#'
+#' If turnout columns sum row-wise exceedingly far from
 #' vote_totals, the function stops and returns an error message.
 #'
 #' @author Ari Decter-Frain
@@ -90,17 +92,16 @@ standardize_votes <- function(votes) {
 #' @param diagnostic A boolean. When true, an extra column of booleans is returned indicating whether each row had a deviation from totals
 #'
 #' @export
-#' 
+#'
 #' @return A dataframe with proportions corresponding to the turnout of each race/ethnicity group
 stdize_votes <- function(
-    data,
-    cols, 
-    totals_col = NULL, 
-    max_dev = 0.1, 
-    avg_dev = 0.025, 
-    verbose = TRUE,
-    diagnostic = FALSE
-) {
+                         data,
+                         cols,
+                         totals_col = NULL,
+                         max_dev = 0.1,
+                         avg_dev = 0.025,
+                         verbose = TRUE,
+                         diagnostic = FALSE) {
 
   # get votes by race, sum of votes
   votes <- data[, cols]
@@ -111,8 +112,8 @@ stdize_votes <- function(
     # if no vote totals_col passed, use vote_sums for totals
     proportions <- standardize_votes(votes)
     return(proportions)
-    
-  # else check the extent of deviation from provided totals
+
+    # else check the extent of deviation from provided totals
   } else {
     vote_totals <- data[, totals_col]
     diff_check <- check_diffs(
@@ -127,10 +128,9 @@ stdize_votes <- function(
       }
       proportions <- standardize_votes(votes)
       if (diagnostic == TRUE) {
-        proportions$deviates <- diff_check$deviates 
+        proportions$deviates <- diff_check$deviates
       }
       return(proportions)
-      
     } else if (closeness == 1) {
       if (verbose == TRUE) {
         message(
@@ -139,21 +139,20 @@ stdize_votes <- function(
       }
       proportions <- standardize_votes(votes)
       if (diagnostic == TRUE) {
-        proportions$deviates <- diff_check$deviates 
+        proportions$deviates <- diff_check$deviates
       }
       return(proportions)
-      
     } else if (closeness == 0) {
       warning(
         "Precinct vote sums are too far from totals.\n  Returning boolean column identifying problematic rows..."
       )
-      return(data.frame('deviates' = diff_check$deviates))
+      return(data.frame("deviates" = diff_check$deviates))
     }
   }
 }
 
 #' Converts a dataframe with total votes for candidates and total votes
-#' by each racial/ethnic group into proportions that can be used for 
+#' by each racial/ethnic group into proportions that can be used for
 #' Ecological Inference analysis
 #'
 #' @author Ari Decter-Frain
@@ -169,78 +168,85 @@ stdize_votes <- function(
 #' @param avg_dec_cand A numeric object setting the max allowable mean deviation of all precincts' sum of candidate columns from totals
 #' @param verbose A boolean. When true, function returns progress messages.
 #' @param diagnostic A boolean. When true, an extra column of booleans is returned indicating whether each row had a deviation from totals
-#' 
+#'
 #' @export
-#' 
+#'
 #' @return A dataframe containing columns for each race and candidate converted to percentages, ready for Ecological Inference
 stdize_votes_all <- function(
-    data,
-    race_cols,
-    cand_cols,
-    totals_from = "cand",
-    totals_col = NULL,
-    max_dev_race = 0.1,
-    max_dev_cand = 0.1,
-    avg_dev_race = 0.025,
-    avg_dev_cand = 0.025,
-    verbose = T,
-    diagnostic = F
-) {
-  
-  if(is.null(totals_col) & totals_from == "cand") {
+                             data,
+                             race_cols,
+                             cand_cols,
+                             totals_from = "cand",
+                             totals_col = NULL,
+                             max_dev_race = 0.1,
+                             max_dev_cand = 0.1,
+                             avg_dev_race = 0.025,
+                             avg_dev_cand = 0.025,
+                             verbose = T,
+                             diagnostic = F) {
+
+  # Use different totals depending on user input.
+  if (is.null(totals_col) & totals_from == "cand") {
     data$cand_totals <- rowSums(data[, cand_cols])
-    cand_totals_col = NULL
-    race_totals_col = "cand_totals"
-  } else if (is.null(totals_col) & totals_from == 'race') {
+    cand_totals_col <- NULL
+    race_totals_col <- "cand_totals"
+  } else if (is.null(totals_col) & totals_from == "race") {
     data$race_totals <- rowSums(data[, race_cols])
-    cand_totals_col = "race_totals"
-    race_totals_col = NULL
+    cand_totals_col <- "race_totals"
+    race_totals_col <- NULL
   } else if (!is.null(totals_col)) {
-    cand_totals_col = totals_col
-    race_totals_col = totals_col
+    cand_totals_col <- totals_col
+    race_totals_col <- totals_col
   } else {
     stop("You must either define a totals column with totals_col, 
        or set totals_from equal to 'cand' or 'race'")
   }
-        
+
+  # get candidate standardized proportions
   cand_prps <- stdize_votes(
     data = data,
-    cols = cand_cols, 
-    totals_col = cand_totals_col, 
-    max_dev = max_dev_cand, 
-    avg_dev = avg_dev_cand, 
-    verbose = verbose,
-    diagnostic = diagnostic  
-  )
-    
-  race_prps <- stdize_votes(
-    data = data,
-    cols = race_cols, 
-    totals_col = race_totals_col, 
-    max_dev = max_dev_race, 
-    avg_dev = avg_dev_race, 
+    cols = cand_cols,
+    totals_col = cand_totals_col,
+    max_dev = max_dev_cand,
+    avg_dev = avg_dev_cand,
     verbose = verbose,
     diagnostic = diagnostic
   )
-  
-  # adjust deviation column names to allow for two 
-  if(names(cand_prps)[1] == 'deviates') {
-    names(cand_prps)[1] <- 'cand_deviates'
+
+  # get race standardized proportions
+  race_prps <- stdize_votes(
+    data = data,
+    cols = race_cols,
+    totals_col = race_totals_col,
+    max_dev = max_dev_race,
+    avg_dev = avg_dev_race,
+    verbose = verbose,
+    diagnostic = diagnostic
+  )
+
+  # adjust deviation column names to allow for two
+  if (names(cand_prps)[1] == "deviates") {
+    names(cand_prps)[1] <- "cand_deviates"
   }
-  if(names(race_prps)[1] == 'deviates') {
-    names(race_prps)[1] <- 'race_deviates'
+  if (names(race_prps)[1] == "deviates") {
+    names(race_prps)[1] <- "race_deviates"
   }
-  if(diagnostic == TRUE) {
-    names(cand_prps)[length(cand_prps)] <- 'cand_deviates'
-    names(race_prps)[length(cand_prps)] <- 'race_deviates'
+  if (diagnostic == TRUE) {
+    names(cand_prps)[length(cand_prps)] <- "cand_deviates"
+    names(race_prps)[length(cand_prps)] <- "race_deviates"
   }
-  
+
   # set appropriate order of columns in output
-  if(totals_from == 'cand') {
+  if (totals_from == "cand") {
     all_proportions <- cbind(cand_prps, race_prps)
   } else {
     all_proportions <- cbind(race_prps, cand_prps)
   }
-  
+
+  # delete extra totals column if there are two
+  if (sum(names(all_proportions) == "total") > 1) {
+    all_proportions <- all_proportions[, -match("total", names(all_proportions))]
+  }
+
   return(all_proportions)
 }
--- tests/testthat/test_ei_preprocessing.R
+++ tests/testthat/test_ei_preprocessing.R
@@ -6,15 +6,15 @@ test_that("standardize_votes() returns correct results", {
   votes$c2 <- c(1, 1)
   totals <- c(2, 2)
   expected <- data.frame(
-    "c1_p" = c(0.5, 0.5), 
-    "c2_p" = c(0.5, 0.5)
+    "c1_p" = c(0.5, 0.5),
+    "c2_p" = c(0.5, 0.5),
+    "total" = c(2, 2)
   )
 
   expect_equal(standardize_votes(votes), expected)
 })
 
 test_that("check_diffs() gets conditions right", {
-  
   vote_sums <- rep(1, 5)
   provided_totals <- rep(1, 5)
   max_dev <- 0.1
@@ -29,7 +29,7 @@ test_that("check_diffs() gets conditions right", {
   )
   expect_equal(res$closeness, 2)
   expect_equal(res$deviates, rep(FALSE, 5))
-  
+
   # near match should return 1
   vote_sums[1] <- 1.05
   res <- check_diffs(
@@ -51,7 +51,7 @@ test_that("check_diffs() gets conditions right", {
   )
   expect_equal(res$closeness, 0)
   expect_equal(res$deviates, c(TRUE, rep(FALSE, 4)))
-  
+
   # avg diff > 0.025 should return 0
   vote_sums <- rep(1.03, 5)
   res <- check_diffs(
@@ -62,20 +62,20 @@ test_that("check_diffs() gets conditions right", {
   )
   expect_equal(res$closeness, 0)
   expect_equal(res$deviates, rep(TRUE, 5))
-  
+
   # should return error if max_dev < 0
   max_dev <- -0.3
   expect_error(check_diffs(
     vote_sums, provided_totals, max_dev, avg_dev
   ))
-  
+
   # should return error if avg_dev < 0
   max_dev <- 0.1
   avg_dev <- -0.3
   expect_error(check_diffs(
     vote_sums, provided_totals, max_dev, avg_dev
   ))
-  
+
   # should return 0 for any deviation if max_dev = 0
   # and avg_dev = 0
   max_dev <- 0
@@ -90,228 +90,237 @@ test_that("check_diffs() gets conditions right", {
 })
 
 test_that("stdize_votes() handles all cases", {
-  
   df <- empty_ei_df()
   df$r1 <- 1
   df$r2 <- 1
   df$t <- 2
-  
+
   # base case works correctly
   res <- stdize_votes(
-    data = df, 
-    cols = c('r1', 'r2'), 
-    totals_col = 't', 
+    data = df,
+    cols = c("r1", "r2"),
+    totals_col = "t",
     verbose = FALSE,
     diagnostic = FALSE
   )
   expected <- data.frame(
-    'r1_p' = c(0.5, 0.5),
-    'r2_p' = c(0.5, 0.5)
+    "r1_p" = c(0.5, 0.5),
+    "r2_p" = c(0.5, 0.5),
+    "total" = c(2, 2)
   )
   expect_equal(res, expected)
-  
+
   # message prints when things work well
   expect_message(
     stdize_votes(
-      data = df, 
-      cols = c('r1', 'r2'), 
-      totals_col = 't', 
+      data = df,
+      cols = c("r1", "r2"),
+      totals_col = "t",
       verbose = TRUE,
       diagnostic = FALSE
     )
   )
-  
+
   # diagnostic works correctly
   res <- stdize_votes(
-    data = df, 
-    cols = c('r1', 'r2'), 
-    totals_col = 't', 
+    data = df,
+    cols = c("r1", "r2"),
+    totals_col = "t",
     verbose = F,
     diagnostic = T
   )
   expected <- data.frame(
-    'r1_p' = c(0.5, 0.5),
-    'r2_p' = c(0.5, 0.5),
-    'deviates' = c(FALSE, FALSE)
+    "r1_p" = c(0.5, 0.5),
+    "r2_p" = c(0.5, 0.5),
+    "total" = c(2, 2),
+    "deviates" = c(FALSE, FALSE)
   )
   expect_equal(res, expected)
-  
+
   # message on minor deviation only where verbose = TRUE
   df$r1[1] <- 0.99
   df$r1[2] <- 1.01
   df$r2[1] <- 1.01
   expect_message(
     stdize_votes(
-      data = df, 
-      cols = c('r1', 'r2'), 
-      totals_col = 't', 
+      data = df,
+      cols = c("r1", "r2"),
+      totals_col = "t",
       verbose = TRUE,
       diagnostic = FALSE
     )
   )
-  
+
   # correct values on minor deviation
   res <- stdize_votes(
-    data = df, 
-    cols = c('r1', 'r2'), 
-    totals_col = 't', 
+    data = df,
+    cols = c("r1", "r2"),
+    totals_col = "t",
     verbose = TRUE,
     diagnostic = FALSE
   )
-  
+
   r11 <- 0.99 / (0.99 + 1.01)
   r12 <- 1.01 / (1 + 1.01)
   r21 <- 1.01 / (0.99 + 1.01)
   r22 <- 1 / (1 + 1.01)
-  
+
   expected <- data.frame(
-    'r1' = c(r11, r12),
-    'r2' = c(r21, r22)
+    "r1_p" = c(r11, r12),
+    "r2_p" = c(r21, r22),
+    "total" = c(2.00, 2.01)
   )
-  
+  expect_equal(expected, res)
+
   # upon violation, print warning
   df$r2 <- 1
-  df$r1 <- c(10,1)
+  df$r1 <- c(10, 1)
   expect_warning(
     stdize_votes(
-      data = df, 
-      cols = c('r1', 'r2'), 
-      totals_col = 't', 
+      data = df,
+      cols = c("r1", "r2"),
+      totals_col = "t",
       verbose = TRUE,
       diagnostic = FALSE
     )
   )
-  
+
   # upon violation, return diagnostic column regardless of verbose
   res <- suppressWarnings({
     stdize_votes(
-      data = df, 
-      cols = c('r1', 'r2'), 
-      totals_col = 't', 
+      data = df,
+      cols = c("r1", "r2"),
+      totals_col = "t",
       verbose = FALSE,
       diagnostic = FALSE
     )
   })
-  expected <- data.frame('deviates' = c(TRUE, FALSE))
+  expected <- data.frame("deviates" = c(TRUE, FALSE))
   expect_equal(res, expected)
 })
 
 test_that("stdize_votes_all() handles all cases", {
   df <- empty_ei_df()
-  df[1,] <- 1
-  df[2,] <- 1
-  
+  df[1, ] <- 1
+  df[2, ] <- 1
+
   # base case works correctly
   res <- stdize_votes_all(
     data = df,
-    race_cols = c('r1', 'r2'),
-    cand_cols = c('c1', 'c2')
+    race_cols = c("r1", "r2"),
+    cand_cols = c("c1", "c2")
   )
   expected <- data.frame(
-    'c1_p' = rep(0.5, 2),
-    'c2_p' = rep(0.5, 2),
-    'r1_p' = rep(0.5, 2),
-    'r2_p' = rep(0.5, 2)
+    "c1_p" = rep(0.5, 2),
+    "c2_p" = rep(0.5, 2),
+    "r1_p" = rep(0.5, 2),
+    "r2_p" = rep(0.5, 2),
+    "total" = c(2, 2)
   )
   expect_equal(res, expected)
-  
+
   # deviation in cand breaks race when totals_from = 'cand'
   df$c1[1] <- 9
   res <- suppressWarnings({
     stdize_votes_all(
       data = df,
-      race_cols = c('r1', 'r2'),
-      cand_cols = c('c1', 'c2')
+      race_cols = c("r1", "r2"),
+      cand_cols = c("c1", "c2")
     )
   })
   expected <- data.frame(
-    'c1_p' = c(0.9, 0.5),
-    'c2_p' = c(0.1, 0.5),
-    'race_deviates' = c(TRUE, FALSE)
+    "c1_p" = c(0.9, 0.5),
+    "c2_p" = c(0.1, 0.5),
+    "total" = c(10, 2),
+    "race_deviates" = c(TRUE, FALSE)
   )
   expect_equal(res, expected)
-  
+
   # deviation in cand breaks cand when totals_from = 'race'
   res <- suppressWarnings({
     stdize_votes_all(
       data = df,
-      race_cols = c('r1', 'r2'),
-      cand_cols = c('c1', 'c2'),
-      totals_from = 'race'
+      race_cols = c("r1", "r2"),
+      cand_cols = c("c1", "c2"),
+      totals_from = "race"
     )
   })
   expected <- data.frame(
-    'r1_p' = c(0.5, 0.5),
-    'r2_p' = c(0.5, 0.5),
-    'cand_deviates' = c(TRUE, FALSE)
+    "r1_p" = c(0.5, 0.5),
+    "r2_p" = c(0.5, 0.5),
+    "total" = c(2, 2),
+    "cand_deviates" = c(TRUE, FALSE)
   )
   expect_equal(res, expected)
-  
+
   # deviation in race breaks cand when totals_from = 'race'
   df$c1[1] <- 1
   df$r1[1] <- 9
   res <- suppressWarnings({
     stdize_votes_all(
       data = df,
-      race_cols = c('r1', 'r2'),
-      cand_cols = c('c1', 'c2'),
-      totals_from = 'race'
+      race_cols = c("r1", "r2"),
+      cand_cols = c("c1", "c2"),
+      totals_from = "race"
     )
   })
   expected <- data.frame(
-    'r1_p' = c(0.9, 0.5),
-    'r2_p' = c(0.1, 0.5),
-    'cand_deviates' = c(TRUE, FALSE)
+    "r1_p" = c(0.9, 0.5),
+    "r2_p" = c(0.1, 0.5),
+    "total" = c(10, 2),
+    "cand_deviates" = c(TRUE, FALSE)
   )
   expect_equal(res, expected)
-  
+
   # deviation in race breaks race when totals_from = 'cand'
   res <- suppressWarnings({
     stdize_votes_all(
       data = df,
-      race_cols = c('r1', 'r2'),
-      cand_cols = c('c1', 'c2'),
-      totals_from = 'cand'
+      race_cols = c("r1", "r2"),
+      cand_cols = c("c1", "c2"),
+      totals_from = "cand"
     )
   })
   expected <- data.frame(
-    'c1_p' = c(0.5, 0.5),
-    'c2_p' = c(0.5, 0.5),
-    'race_deviates' = c(TRUE, FALSE)
+    "c1_p" = c(0.5, 0.5),
+    "c2_p" = c(0.5, 0.5),
+    "total" = c(2, 2),
+    "race_deviates" = c(TRUE, FALSE)
   )
   expect_equal(res, expected)
-  
+
   # totals_col works in base case
   df$r1 <- 1
   df$t <- 2
-  res <- suppressWarnings({
+  res <- suppressMessages({
     stdize_votes_all(
       data = df,
-      race_cols = c('r1', 'r2'),
-      cand_cols = c('c1', 'c2'),
-      totals_col = 't'
+      race_cols = c("r1", "r2"),
+      cand_cols = c("c1", "c2"),
+      totals_col = "t"
     )
   })
   expected <- data.frame(
-    'c1_p' = rep(0.5, 2),
-    'c2_p' = rep(0.5, 2),
-    'r1_p' = rep(0.5, 2),
-    'r2_p' = rep(0.5, 2)
+    "c1_p" = rep(0.5, 2),
+    "c2_p" = rep(0.5, 2),
+    "r1_p" = rep(0.5, 2),
+    "r2_p" = rep(0.5, 2),
+    "total" = c(2, 2)
   )
   expect_equal(res, expected)
-  
+
   # both break when a total deviates
   df$t[1] <- 10
   res <- suppressWarnings({
     stdize_votes_all(
       data = df,
-      race_cols = c('r1', 'r2'),
-      cand_cols = c('c1', 'c2'),
-      totals_col = 't'
+      race_cols = c("r1", "r2"),
+      cand_cols = c("c1", "c2"),
+      totals_col = "t"
     )
   })
   expected <- data.frame(
-    'cand_deviates' = c(TRUE, FALSE),
-    'race_deviates' = c(TRUE, FALSE)
+    "cand_deviates" = c(TRUE, FALSE),
+    "race_deviates" = c(TRUE, FALSE)
   )
 })
