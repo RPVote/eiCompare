a8d1956c910c48da76a7a7c348628abef846fbea
hikari-murayama <hikari_murayama@berkeley.edu>
2020-08-03 18:55:34 +0000

    integrate overlays and pass R CMD

--- R/ei_iter.R
+++ R/ei_iter.R
@@ -44,6 +44,10 @@
 #' @export
 #'
 #' @return dataframe of results from iterative ei
+#'
+#'
+utils::globalVariables(c("%dopar%", "%do%", "i", "betas_ei"))
+
 ei_iter <- function(
                     data,
                     cand_cols,
