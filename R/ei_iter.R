383cfeebeb46e701b3914ba1dc7124dcab24fc6b
hikari-murayama <hikari_murayama@berkeley.edu>
2020-08-04 19:41:31 +0000

    added tomography and beta comparison plots

--- R/ei_iter.R
+++ R/ei_iter.R
@@ -141,10 +141,28 @@ ei_iter <- function(
         )
     })
 
-    # # Plots to be added here
-    # if (plots) {
-    #   do_nothing <- 3
-    # }
+    # Plots to be added here
+    if (plots) {
+      # Create tomography plots
+      png(paste0(path, "tomography_", cand, "_", race, ".png"),
+        units = "in", height = 6, width = 6, res = 500
+      )
+      plot(ei_out, "tomogE")
+      graphics::mtext(paste(cand, race, sep = " "),
+        outer = T, line = -1
+      )
+      dev.off()
+
+      # Create denity plots
+      png(paste0(path, "density_", cand, "_", race, ".png"),
+        units = "in", height = 6, width = 6, res = 500
+      )
+      plot(ei_out, "betab", "betaw")
+      graphics::mtext(paste(cand, race, sep = " "),
+        outer = T, line = -1
+      )
+      dev.off()
+    }
 
     # Extract mean, standard error for each precinct
     precinct_res <- ei::eiread(
