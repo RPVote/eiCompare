7d05da1f4020d285cd921aff22718dee3bec00bc
aridf <adecterfrain@gmail.com>
2020-08-05 18:39:57 +0000

    get rid of tidyr import in plot_bivar()

--- R/plot_bivariate.R
+++ R/plot_bivariate.R
@@ -1,7 +1,6 @@
 #' Plot bivariate relationships
 #'
 #' @import ggplot2
-#' @importFrom tidyr pivot_longer
 #'
 #' @param data A data.frame() object containing precinct-level turnout data by
 #' race and candidate
@@ -14,22 +13,61 @@
 #'
 #' @return ggplot object with bivariate plots faceted by candidate and race
 plot_bivariate <- function(data, cand_cols, race_cols) {
-  cols <- length(race_cols)
-  rows <- length(cand_cols)
-  dot_size <- min(4 / log(cols * rows), 3)
-  data_for_plot <- data %>%
-    tidyr::pivot_longer(
-      cand_cols,
-      names_to = "candidate",
-      values_to = "pct_of_vote"
-    ) %>%
-    tidyr::pivot_longer(
-      race_cols,
-      names_to = "race",
-      values_to = "pct_of_voters"
-    )
+  data <- data[, c(cand_cols, race_cols)]
+
+  n_races <- length(race_cols)
+  n_cands <- length(cand_cols)
+  dot_size <- min(4 / log(n_races * n_cands), 3)
+
+  # Lengthen data over candidates
+  cand_long <- as.data.frame(matrix(ncol = n_races + 2, nrow = 0))
+  for (i in 1:n_cands) {
+    cand <- cand_cols[i]
+
+    # Drop other candidate columns
+    other_cands <- cand_cols[which(cand_cols != cand)]
+    temp <- data[, -which(names(data) %in% other_cands)]
+
+    # Get correct colname
+    colnames(temp)[which(colnames(temp) == cand)] <- "pct_of_vote"
+    temp$candidate <- cand
+
+    # Add temp to dataframe
+    cand_long <- rbind(cand_long, temp)
+  }
+
+  # Lengthen data over races
+  data_long <- as.data.frame(matrix(ncol = 4, nrow = 0))
+  for (i in 1:n_races) {
+    race <- race_cols[i]
+
+    # Drop other race columns
+    other_races <- race_cols[which(race_cols != race)]
+    temp <- cand_long[, -which(names(cand_long) %in% other_races)]
+
+    # Get correct colname
+    colnames(temp)[which(colnames(temp) == race)] <- "pct_of_voters"
+    temp$race <- race
+
+    # Add temp to dataframe
+    data_long <- rbind(data_long, temp)
+  }
+
+
+  # data_for_plot <- data %>%
+  #  tidyr::pivot_longer(
+  #    cand_cols,
+  #    names_to = "candidate",
+  #    values_to = "pct_of_vote"
+  #  ) %>%
+  #  tidyr::pivot_longer(
+  #    race_cols,
+  #    names_to = "race",
+  #    values_to = "pct_of_voters"
+  #  )
+
   ggplot2::ggplot(
-    data = data_for_plot,
+    data = data_long,
     aes(x = pct_of_voters, y = pct_of_vote)
   ) +
     ggplot2::geom_point(alpha = 0.5, size = dot_size) +
