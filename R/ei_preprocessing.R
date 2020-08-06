4496750c97eac8da2d50ef808d9a29756a1e5a9a
Ari Decter-Frain <adecterfrain@gmail.com>
2020-08-06 19:27:43 -0400

    More EI preprocessing functions (#56)
    * first commit
    
    * complete unit testing for check_race_diffs()
    
    * add return_devs param to clean_race
    
    * add clean_race() unit tests
    
    * generalize clean_race() to clean_votes()
    
    * rename functions
    
    * add stdize_votes_all()
    
    * bugfixes
    
    * add stdize_votes_all() tests
    
    * style
    
    * style
    
    * updates from code review
    
    * change commands.yml version to 3
    
    * change version to 1
    
    * update docs
    
    * disable r-cmd-check-cran
    
    * plot_bivar() and dedupe_precincts()
    
    * update docstring for plot_bivar()
    
    * add check_missing()
    
    * export plot_bivariate and check_missing
    
    * export preprocessing and remove mutate import from plot_bivar
    
    * update message line justification
    
    * get rid of tidyr import in plot_bivar()
    
    * code review changes
    
    * fix bug due to tibble vs df, add new_names param to stdize_votes_all()

--- R/ei_preprocessing.R
+++ R/ei_preprocessing.R
@@ -1,3 +1,151 @@
+#' Remove or identify duplicated precincts
+#'
+#' Removes any rows in the dataset that are fully duplicated. If necessary, adds
+#'' duplicates' column indicating where precincts appear duplicated, for manual
+#' inspection by the user
+#'
+#' @author Ari Decter-Frain <agd75@@cornell.edu>
+#'
+#' @param data A data.frame() object containing precinct-level turnout data by
+#' race and candidate
+#' @param id_cols The name or index of the column in the data containing
+#' unique precinct identifiers. Can pass multiple column names or indices in
+#' a vector if precincts are identified over multiple columns
+#' (eg. c("precinctid", "countyid")).
+#' @param verbose A boolean. If true, messages are returned describing actions
+#' taken by the function.
+#'
+#' @export
+#'
+#' @return A new dataframe without duplicated rows, and (if any) a boolean
+#' column identifying duplicated precincts for further investigation.
+
+dedupe_precincts <- function(data, id_cols, verbose = TRUE) {
+
+  # Remove any fully duplicated rows
+  init_rows <- nrow(data)
+  data <- unique(data)
+  rows <- nrow(data)
+  rows_removed <- init_rows - rows
+
+  # Inform user
+  if (rows_removed != 0 & verbose) {
+    message(paste("Removing", rows_removed, "identical rows..."))
+  }
+
+  # Check for duplicate precincts
+  # Check both directions to get every duplicate
+  dupes <- duplicated(data[, id_cols]) |
+    duplicated(data[, id_cols],
+      fromLast = TRUE
+    )
+
+  # If duplicates found, add boolean column identifying them
+  if (sum(dupes) != 0 & verbose) {
+    data$duplicate <- dupes
+
+    # Inform user
+    warning("Precincts appear duplicated. Returning boolean column identifying
+duplicates...")
+  }
+
+  # Inform user if no duplicates
+  if ((rows_removed == 0) & (sum(dupes) == 0) & verbose) {
+    message("Data does not contain duplicates. Proceed...")
+  }
+
+  return(data)
+}
+
+#' Remove / Impute NAs in an EI dataset
+#'
+#' @param data A data.frame() object containing precinct-level turnout data by
+#' race and candidate
+#' @param cand_cols A character vector listing the column names for turnout for
+#' each candidate
+#' @param race_cols A character vector listing the column names for turnout by
+#' race
+#' @param totals_col The name of the column containing total votes cast in each
+#' precinct
+#' @param na_action A string indicating how to handle missing values in EI
+#' columns. Possible values are "DROP" and "MEAN". "DROP" drops all rows
+#' where variables are missing. "MEAN" imputes missing values as the mean of the
+#' column
+#' @param verbose A boolean indicating whether to give status updates
+#'
+#' @export
+resolve_missing_vals <- function(
+                                 data,
+                                 cand_cols,
+                                 race_cols,
+                                 totals_col,
+                                 na_action = "DROP",
+                                 verbose = TRUE) {
+  cols <- c(cand_cols, race_cols, totals_col)
+  na_action <- toupper(na_action)
+
+  # If "drop" selected
+  if (na_action == "DROP") {
+    n_rows <- nrow(data)
+
+    # Drop rows with NAs in any necessary column
+    data <- data[rowSums(is.na(data[, cols])) == 0, ]
+    n_rows_new <- nrow(data)
+    n_dropped <- n_rows - n_rows_new
+    if (verbose) {
+      if (n_dropped == 0) {
+        message(
+          "No missing values in key columns. Returning original dataframe..."
+        )
+      } else {
+        message(
+          paste(
+            "Removing", n_dropped, "rows with missing values in key columns..."
+          )
+        )
+      }
+    }
+
+    # If "mean" selected
+  } else if (na_action == "MEAN") {
+    ei_data <- data[, cols]
+    n_missing <- 0
+
+    for (i in 1:ncol(ei_data)) {
+      missing <- is.na(ei_data[, i])
+
+      # Update counter
+      n_missing <- n_missing + sum(missing)
+
+      # Replace missings with column mean
+      ei_data[missing, i] <- mean(ei_data[, i], na.rm = TRUE)
+    }
+
+    # Put imputed data back into dataset
+    data[, cols] <- ei_data
+
+    if (verbose) {
+      if (n_missing == 0) {
+        message(
+          "No missing values in key columns. Returning original dataframe..."
+        )
+      } else {
+        message(
+          paste(
+            "Imputing", n_missing, "missing values across key columns..."
+          )
+        )
+      }
+    }
+  } else {
+    stop(
+      "Invalid entry for na_action parameter.\nEnter either 'DROP' or 'MEAN'"
+    )
+  }
+  return(data)
+}
+
+
 #' Internal function that checks for adequate closeness between sums
 #' of race/candidate columns and provided vote totals.
 #'
@@ -61,15 +209,19 @@ check_diffs <- function(vote_sums,
 #' @author Ari Decter-Frain
 #'
 #' @param votes A dataframe of raw votes by race or candidates
+#' @param new_names A boolean indicating whether to return cand and race columns
+#' with the same names. If FALSE, names returned with "_prop" added on.
 #'
 #' @keywords internal
 #'
 #' @return A dataframe of standardized proportions whose columns sum rowwise to
 #' 1
-standardize_votes <- function(votes) {
+standardize_votes <- function(votes, new_names = FALSE) {
   totals <- rowSums(votes)
   prps <- votes / totals
-  names(prps) <- paste(names(prps), "prop", sep = "_")
+  if (new_names) {
+    names(prps) <- paste(names(prps), "prop", sep = "_")
+  }
   output <- cbind(prps, data.frame("total" = totals))
   return(output)
 }
@@ -102,6 +254,8 @@ standardize_votes <- function(votes) {
 #' precinct's vote sum from totals
 #' @param avg_dev A numeric object setting the max allowable average deviation
 #' difference of all precints' vote sums from totals
+#' @param new_names A boolean indicating whether to return cand and race columns
+#' with the same names. If FALSE, names returned with "_prop" added on.
 #' @param verbose A boolean indicating whether to print status messages
 #' @param diagnostic A boolean. When true, an extra column of booleans is
 #' returned indicating whether each row had a deviation from totals
@@ -115,20 +269,24 @@ stdize_votes <- function(data,
                          totals_col = NULL,
                          max_dev = 0.1,
                          avg_dev = 0.025,
+                         new_names = FALSE,
                          verbose = TRUE,
                          diagnostic = FALSE) {
 
-  # get votes by race, sum of votes
+  # Set data as dataframe
+  data <- as.data.frame(data)
+
+  # Get votes by race, sum of votes
   votes <- data[, cols]
   vote_sums <- rowSums(votes)
 
   if (is.null(totals_col)) {
 
-    # if no vote totals_col passed, use vote_sums for totals
-    proportions <- standardize_votes(votes)
+    # If no vote totals_col passed, use vote_sums for totals
+    proportions <- standardize_votes(votes, new_names)
     return(proportions)
 
-    # else check the extent of deviation from provided totals
+    # Else check the extent of deviation from provided totals
   } else {
     vote_totals <- data[, totals_col]
     diff_check <- check_diffs(
@@ -141,7 +299,7 @@ stdize_votes <- function(data,
           "All columns sum correctly. Computing proportions..."
         )
       }
-      proportions <- standardize_votes(votes)
+      proportions <- standardize_votes(votes, new_names)
       if (diagnostic) {
         proportions$deviates <- diff_check$deviates
       }
@@ -149,19 +307,23 @@ stdize_votes <- function(data,
     } else if (closeness == 1) {
       if (verbose) {
         message(
-          "Vote sums deviate from totals.\nDeviations are minor. 
-          Restandardizing vote columns..."
+          paste(
+            "Vote sums deviate from totals.\nDeviations are minor.",
+            "Restandardizing vote columns..."
+          )
         )
       }
-      proportions <- standardize_votes(votes)
+      proportions <- standardize_votes(votes, new_names)
       if (diagnostic) {
         proportions$deviates <- diff_check$deviates
       }
       return(proportions)
     } else if (closeness == 0) {
       warning(
-        "Precinct vote sums are too far from totals.\n  
-        Returning boolean column identifying problematic rows..."
+        paste(
+          "Precinct vote sums are too far from totals.\n",
+          "Returning boolean column identifying problematic rows..."
+        )
       )
       return(data.frame("deviates" = diff_check$deviates))
     }
@@ -195,6 +357,8 @@ stdize_votes <- function(data,
 #' of all precincts' sum of race columns from totals
 #' @param avg_dev_cand A numeric object setting the max allowable mean deviation
 #' of all precincts' sum of candidate columns from totals
+#' @param new_names A boolean indicating whether to return cand and race columns
+#' with the same names. If FALSE, names returned with "_prop" added on.
 #' @param ignore_devs A boolean. When true, columns are standardized ignoring
 #' all deviations from totals
 #' @param verbose A boolean. When true, function returns progress messages.
@@ -214,6 +378,7 @@ stdize_votes_all <- function(data,
                              max_dev_cand = 0.1,
                              avg_dev_race = 0.025,
                              avg_dev_cand = 0.025,
+                             new_names = FALSE,
                              ignore_devs = FALSE,
                              verbose = TRUE,
                              diagnostic = FALSE) {
@@ -240,8 +405,12 @@ stdize_votes_all <- function(data,
     cand_totals_col <- totals_col
     race_totals_col <- totals_col
   } else {
-    stop("You must either define a totals column with totals_col, 
-       or set totals_from equal to 'cand' or 'race'")
+    stop(
+      paste(
+        "You must either define a totals column with totals_col,\n",
+        "or set totals_from equal to 'cand' or 'race'"
+      )
+    )
   }
 
   # If ignore devs, set to Inf to pass checks
@@ -259,6 +428,7 @@ stdize_votes_all <- function(data,
     totals_col = cand_totals_col,
     max_dev = max_dev_cand,
     avg_dev = avg_dev_cand,
+    new_names = new_names,
     verbose = verbose,
     diagnostic = diagnostic
   )
@@ -270,6 +440,7 @@ stdize_votes_all <- function(data,
     totals_col = race_totals_col,
     max_dev = max_dev_race,
     avg_dev = avg_dev_race,
+    new_names = new_names,
     verbose = verbose,
     diagnostic = diagnostic
   )
