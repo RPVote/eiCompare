dcb231d909f529924f70ed3fb52984edfcdaf15a
hikari-murayama <hikari_murayama@berkeley.edu>
2020-08-06 19:57:22 +0000

    use aggregate values for plotting

--- R/ei_iter.R
+++ R/ei_iter.R
@@ -86,7 +86,7 @@ ei_iter <- function(
   check_args(data, cand_cols, race_cols, totals_col)
 
   # Save any additional arguments to pass into ei inside foreach
-  # args_pass <- list(...)
+  args_pass <- list(...)
 
   # Subset data
   data <- data[, c(cand_cols, race_cols, totals_col)]
@@ -272,6 +272,7 @@ ei_iter <- function(
 
     agg_betas <- data.frame(do.call(cbind, agg_results))
     colnames(agg_betas) <- new_colnames
+
 
     # Create density plots
     density_plots <- overlay_density_plot(agg_betas, results_table, plot_path, ei_type = "ei")
