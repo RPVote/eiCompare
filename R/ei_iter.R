5494ba49f851869e703e32de7fb6412f3ea6ee23
hikari-murayama <hikari_murayama@berkeley.edu>
2020-08-06 22:44:22 +0000

    edit needed packages and namespace issues

--- R/ei_iter.R
+++ R/ei_iter.R
@@ -25,7 +25,7 @@
 #' @param plot_path A string to specify plot save location. Defaulted to working directory
 #' @param ... Additional arguments passed directly to ei::ei()
 #'
-#' @importFrom doSNOW registerDoSNOW getDoParWorkers
+#' @importFrom doSNOW registerDoSNOW getDoParWorkers foreach
 #' @importFrom purrr lift
 #' @importFrom utils capture.output setTxtProgressBar
 #'
@@ -40,7 +40,7 @@
 #' @return dataframe of results from iterative ei
 #'
 #'
-utils::globalVariables(c("%dopar%", "%do%", "i"))
+utils::globalVariables(c("`%dopar%`", "`%do%`", "i"))
 
 ei_iter <- function(
                     data,
