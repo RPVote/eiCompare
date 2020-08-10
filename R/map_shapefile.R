8d7c3351e9f01d5915ed27bac2f6884bd5ead09a
Juandalyn Burke <jcburke@uw.edu>
2020-08-10 16:55:55 +0000

    Added updates to map_shapefile.R for returning a generic map of an ecological unit and mapping voter latitude and longitude values.

---
+++ R/map_shapefile.R
@@ -0,0 +1,88 @@
+#' Function for making basic choropleth maps using the tigris package and ggplot
+#'
+#' @param voter_file a voter file with latitude, longitude, or latitude/longitude and other geographic data.
+#' @param shape_file a shape file based on desired ecologicla unit (i.e. state, county, block, tract)
+#' @param crs the Coordinate reference system, default is crs="+proj=latlong +ellps=GRS80 +no_defs"
+#' @param title the tile of the map
+#'
+#' @export
+#'
+#' @importFrom sf
+#' @importFrom ggplot2
+#' @importFrom ggmap
+
+map_shape_file <- function(shape_file,
+                           crs = "+proj=latlong +ellps=GRS80 +no_defs",
+                           title = "Title of the Shapefile") {
+  # Map the shapefile only and label ecological unit
+  if (!is.null(shape_file)) {
+    # Transform shape_file
+    shape_file <- st_transform(shape_file, crs = crs)
+
+    suppressMessages(
+      sf_points <- st_centroid(shape_file)
+    )
+
+    suppressMessages(
+      sf_points <- cbind(shape_file, st_coordinates(st_centroid(shape_file$geometry)))
+    )
+
+    shape_file <- shape_file %>%
+      ggplot() +
+      geom_sf() +
+      geom_text(
+        data = sf_points, aes(x = X, y = Y, label = NAME),
+        color = "darkblue", fontface = "bold", check_overlap = FALSE
+      ) +
+      labs(title) +
+      theme_minimal()
+  }
+  return(shape_file)
+}
+
+
+map_shape_points <- function(voter_file,
+                             shape_file,
+                             crs = "+proj=longlat +ellps=GRS80",
+                             title = "title") {
+
+  # Create lat and lon coordinates from geometry column of the full geometry voter file output
+  voter_file_geo_latlon <- voter_file %>%
+    extract(geometry, c("lat", "lon"), "\\((.*), (.*)\\)", convert = TRUE)
+
+  # Transform to sf
+  voter_file_geo_latlon <- st_as_sf(x = voter_file_geo_latlon, coords = 45:46)
+
+
+  # Establish Coordinate Reference System (CRS)
+  crs <- "+proj=longlat +ellps=GRS80"
+  voter_file_geo_latlon <- sf::st_transform(sf::st_set_crs(voter_file_geo_latlon, crs))
+  voter_file_geo_latlon <- st_transform(voter_file_geo_latlon, crs = st_crs(shape_file))
+  st_crs(voter_file_geo_latlon) <- st_crs(shape_file)
+
+  # Merge files
+  voter_file_geo_onlyint <- st_intersection(voter_file_geo_latlon, shape_file)
+
+  # Distinguish between the different counties
+  shape_file_polygons <- voter_file_geo_onlyint %>%
+    ggplot() +
+    geom_sf(aes(fill = factor(COUNTYFP10))) +
+    geom_sf(data = shape_file, aes(fill = factor(fips))) +
+    labs(fill = "COUNTYFP10") +
+    theme_minimal()
+
+  # Map the geometry points over the shapefile
+  shape_file_points <- ggplot() +
+    geom_sf(data = shape_file, fill = "gray36") +
+    geom_sf(
+      data = voter_file_geo_onlyint,
+      col = "blue1",
+      size = .0005
+    ) +
+    scale_fill_grey(start = 0.3, end = 0.3) +
+    labs(title = title)
+
+  shape_list <- list(shape_file_polygons, shape_file_points)
+
+  return(shape_list)
+}
