b94d8013b32cc3830cc1ad9207d860d75d4eab3e
Juandalyn Burke <jcburke@uw.edu>
2020-08-11 16:48:24 +0000

    Updated run_geocoder() and revised vignettes/geocoding for typos, etc. in the file

--- R/run_geocoder.R
+++ R/run_geocoder.R
@@ -64,7 +64,6 @@ run_geocoder <- function(voter_file,
       zip = zipcode,
       return = census_return,
       benchmark = census_benchmark,
-      vintage = census_vintage,
       output = census_output,
       class = census_class
     )
@@ -90,18 +89,20 @@ run_geocoder <- function(voter_file,
     }
 
     if (n_cores >= 4) {
-      # Start-up message
-      message("Running US Census Geocoding API. This may take several minutes (i.e. up to 20 minutes) to complete.")
+
+      # Start-up messages
+      message("Running US Census Geocoding API.")
 
       packageStartupMessage("Initializing...", appendLF = FALSE)
 
+      # Run Census Geocoder API
       clust <- makeCluster(detectCores() - 2)
       registerDoParallel(clust)
 
       start_time_2 <- Sys.time()
 
-      census_voter_file <- foreach(i = 1:1, .combine = rbind, .packages = c("censusxy", "sf")) %dopar% {
-        census_voter_file <- cxy_geocode(
+      census_voter_file <- foreach(i = 1, .combine = rbind, .packages = c("censusxy", "sf")) %dopar% {
+        cxy_geocode(
           .data = voter_file,
           id = voter_id,
           street = street,
@@ -114,134 +115,140 @@ run_geocoder <- function(voter_file,
           output = census_output,
           class = census_class
         )
-
-        voter_file <- census_voter_file
-
-        # Print message "done"
-        packageStartupMessage("done")
-
-        return(voter_file)
-      }
-    }
-
-    #################################################################################################
-    # Runs ONLY if Census Geocoder API is selected, the user is conducting parallel processing and  #
-    # there are more than 10,000 observations/voters.                                               #
-    #################################################################################################
-
-    if (geocoder == "census" & parallel == TRUE & num_obs > 10000) {
-      # Detect the number of cores on user's machine
-      n_cores <- detectCores()
-
-      # Send warning message if number of cores is less than 4 cores
-      if (n_cores < 4) {
-        message("Your machine is has less than 4 cores and will not be able to perform parallel processing.")
-        packageStartupMessage("Aborting parallel processing option...", appendLF = FALSE)
-        Sys.sleep(1)
-        packageStartupMessage(" done")
       }
 
-      if (n_cores >= 4) {
+      (end_time_2 <- Sys.time() - start_time_2)
 
-        # Start-up messages
-        message("Running US Census Geocoding API.")
+      stopCluster(clust)
+      gc()
 
-        packageStartupMessage("Initializing...", appendLF = FALSE)
+      voter_file <- census_voter_file
 
-        # Set up iterations for parallel processing
-        # Set number of loops needed to batch every 10000 rows of the voter file
-        n_loops <- round(num_obs / 10000, digits = 0) + 1
-        start_row <- 1
-        stop_row <- start_row + 9999
-        last_row_stop <- num_obs
-        last_row_start <- last_row_stop - (num_obs %% 10000) + 1
+      Sys.sleep(1)
+      packageStartupMessage(" done")
 
-        # Create empty list that will hold batches of data by 10K observations.
-        dfList <- list()
-
-        # Creates lists of dataframes that holds voter data in batches of 10000
-        for (i in 1:n_loops) {
-          df <- voterf_file[start_row:stop_row, ]
-          if (start_row == last_row_start) {
-            df <- voter_file[last_row_start:last_row_stop, ]
-          }
-          dfList <- append(dfList, list(df))
-          start_row <- stop_row + 1
-          stop_row <- start_row + 9999
-        }
-
-        # Run Census Geocoder API
-        clust <- makeCluster(detectCores() - 2)
-        registerDoParallel(clust)
-
-        start_time_2 <- Sys.time()
-
-        census_voter_file <- foreach(i = 1:n_loops, .combine = rbind, .packages = c("censusxy", "sf")) %dopar% {
-          cxy_geocode(
-            .data = dfList[[i]],
-            id = voter_id,
-            street = street,
-            city = city,
-            state = state,
-            zip = zipcode,
-            return = census_return,
-            benchmark = census_benchmark,
-            vintage = census_vintage,
-            output = census_output,
-            class = census_class
-          )
-        }
-
-        (end_time_2 <- Sys.time() - start_time_2)
-
-        stopCluster(clust)
-        gc()
-
-        voter_file <- census_voter_file
-
-        Sys.sleep(1)
-        packageStartupMessage(" done")
-
-        return(voter_file)
-      }
+      return(voter_file)
     }
   }
 
-  ############################################################################
-  # Runs ONLY if Opencage geocoder is selected and there is an opencage_key. #
-  ############################################################################
 
-  if (geocoder == "opencage" & !is.null(opencage_key)) {
 
-    # format address to opencage best pratice for addresses using commas
-    voter_file$opencage_address <- paste(voter_file[[street]],
-      voter_file[[city]],
-      voter_file[[state]],
-      voter_file[[zipcode]],
-      sep = ","
-    )
-    # Geocode using new address format for opencage and create columns for geographies
-    for (m in 1:num_obs) {
-      tryCatch(
-        {
-          opencage_latlon <- opencage_forward(
-            placename = voter_file$opencage_address[m],
-            country = NULL,
-            key = opencage_key
-          )
-          voter_file$lon[m] <- opencage_latlon$results$geometry.lng[1] # longtiude
-          voter_file$lat[m] <- opencage_latlon$results$geometry.lat[1] # latitude
-          voter_file$confidence[m] <- opencage_latlon$results$confidence[1] # confidence score
-          voter_file$address[m] <- opencage_latlon$results$formatted[1] # opencage address
-          voter_file$county[m] <- opencage_latlon$results$components.county[1] # county name
-          voter_file$county_fips[m] <- opencage_latlon$results$annotations.FIPS.county[1] # FIPS county code
-        },
-        error = function(e) {
-          print(e)
-        }
-      )
+  #################################################################################################
+  # Runs ONLY if Census Geocoder API is selected, the user is conducting parallel processing and  #
+  # there are more than 10,000 observations/voters.                                               #
+  #################################################################################################
+
+  if (geocoder == "census" & parallel == TRUE & num_obs > 10000) {
+    # Detect the number of cores on user's machine
+    n_cores <- detectCores()
+
+    # Send warning message if number of cores is less than 4 cores
+    if (n_cores < 4) {
+      message("Your machine is has less than 4 cores and will not be able to perform parallel processing.")
+      packageStartupMessage("Aborting parallel processing option...", appendLF = FALSE)
+      Sys.sleep(1)
+      packageStartupMessage(" done")
     }
 
+    if (n_cores >= 4) {
+
+      # Start-up messages
+      message("Running US Census Geocoding API.")
+
+      packageStartupMessage("Initializing...", appendLF = FALSE)
+
+      # Set up iterations for parallel processing
+      # Set number of loops needed to batch every 10000 rows of the voter file
+      n_loops <- round(num_obs / 10000, digits = 0) + 1
+      start_row <- 1
+      stop_row <- start_row + 9999
+      last_row_stop <- num_obs
+      last_row_start <- last_row_stop - (num_obs %% 10000) + 1
+
+      # Create empty list that will hold batches of data by 10K observations.
+      dfList <- list()
+
+      # Creates lists of dataframes that holds voter data in batches of 10000
+      for (i in 1:n_loops) {
+        df <- voter_file[start_row:stop_row, ]
+        if (start_row == last_row_start) {
+          df <- voter_file[last_row_start:last_row_stop, ]
+        }
+        dfList <- append(dfList, list(df))
+        start_row <- stop_row + 1
+        stop_row <- start_row + 9999
+      }
+
+      # Run Census Geocoder API
+      clust <- makeCluster(detectCores() - 2)
+      registerDoParallel(clust)
+
+      start_time_2 <- Sys.time()
+
+      census_voter_file <- foreach(i = 1:n_loops, .combine = rbind, .packages = c("censusxy", "sf")) %dopar% {
+        cxy_geocode(
+          .data = dfList[[i]],
+          id = voter_id,
+          street = street,
+          city = city,
+          state = state,
+          zip = zipcode,
+          return = census_return,
+          benchmark = census_benchmark,
+          vintage = census_vintage,
+          output = census_output,
+          class = census_class
+        )
+      }
+
+      (end_time_2 <- Sys.time() - start_time_2)
+
+      stopCluster(clust)
+      gc()
+
+      voter_file <- census_voter_file
+
+      Sys.sleep(1)
+      packageStartupMessage(" done")
+
+      return(voter_file)
+    }
+
+    ############################################################################
+    # Runs ONLY if Opencage geocoder is selected and there is an opencage_key. #
+    ############################################################################
+
+    if (geocoder == "opencage" & !is.null(opencage_key)) {
+
+      # format address to opencage best pratice for addresses using commas
+      voter_file$opencage_address <- paste(voter_file[[street]],
+        voter_file[[city]],
+        voter_file[[state]],
+        voter_file[[zipcode]],
+        sep = ","
+      )
+      # Geocode using new address format for opencage and create columns for geographies
+      for (m in 1:num_obs) {
+        tryCatch(
+          {
+            opencage_latlon <- opencage_forward(
+              placename = voter_file$opencage_address[m],
+              country = NULL,
+              key = opencage_key
+            )
+            voter_file$lon[m] <- opencage_latlon$results$geometry.lng[1] # longtiude
+            voter_file$lat[m] <- opencage_latlon$results$geometry.lat[1] # latitude
+            voter_file$confidence[m] <- opencage_latlon$results$confidence[1] # confidence score
+            voter_file$address[m] <- opencage_latlon$results$formatted[1] # opencage address
+            voter_file$county[m] <- opencage_latlon$results$components.county[1] # county name
+            voter_file$county_fips[m] <- opencage_latlon$results$annotations.FIPS.county[1] # FIPS county code
+          },
+          error = function(e) {
+            print(e)
+          }
+        )
+      }
+    }
     return(voter_file)
   }
 }
