8797fe38c597d7a0e57cb977dd2d46db540e212e
aridf <adecterfrain@gmail.com>
2020-08-18 19:55:35 +0000

    bugfix stdize_votes_all

--- R/ei_preprocessing.R
+++ R/ei_preprocessing.R
@@ -497,7 +497,11 @@ stdize_votes_all <- function(data,
   # Rename totals column to the original inputed name
   if (!new_names) {
     totals_index <- which(colnames(all_proportions) == "total")
-    colnames(all_proportions)[totals_index] <- totals_col
+    colnames(all_proportions)[totals_index] <- ifelse(
+      is.null(totals_col),
+      "total",
+      totals_col
+    )
   }
 
   return(all_proportions)
