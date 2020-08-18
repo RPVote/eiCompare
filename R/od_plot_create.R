19392715cb0bc9b1b69569534d168d646c4932bd
hikari-murayama <hikari_murayama@berkeley.edu>
2020-08-18 02:07:46 +0000

    alternate method for combining ggplot2 scales

--- R/od_plot_create.R
+++ R/od_plot_create.R
@@ -46,9 +46,16 @@ od_plot_create <- function(race, cand_pair, dens_data, out, plot_path = "", cand
   )
   names(cols) <- c(gsub("pct_", "", cand_pair[1]), gsub("pct_", "", cand_pair[2]))
 
+  # Set fill and color aesthetic ahead of time
+  scale_aes <- list(
+    scale_fill_manual(values = cols),
+    scale_color_manual(values = cols)
+  )
+
+
   densplot <- ggplot2::ggplot(dens_data_sub, ggplot2::aes(x = value, fill = Candidate)) +
     # Set colors according to candidate
-    scale_fill_manual(values = cols) +
+    scale_aes +
     # Add titles
     ggplot2::ggtitle(paste0(
       gsub("pct_", "", cand_pair[1]), " vs ",
@@ -59,8 +66,8 @@ od_plot_create <- function(race, cand_pair, dens_data, out, plot_path = "", cand
     ggplot2::xlab("Percent of vote") +
     ggplot2::ylab("Density") +
     ggplot2::geom_density(
-      alpha = 0.8, ggplot2::aes(x = value * 100, y = ..scaled..),
-      color = NA, adjust = 2
+      alpha = 0.5, ggplot2::aes(x = value * 100, y = ..scaled..),
+      adjust = 2
     ) +
     # Add vertical line for halfway
     ggplot2::geom_vline(xintercept = 50, color = "black", linetype = "dotted") +
