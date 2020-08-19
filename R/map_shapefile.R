42914a9673cb220c7b67f95f11ac431c8870aa84
Juandalyn Burke <jcburke@uw.edu>
2020-08-19 00:13:18 +0000

    Added tidyr package to import in map_shapefile.R and changes the importFrom to @import.

--- R/map_shapefile.R
+++ R/map_shapefile.R
@@ -1,18 +1,17 @@
-#' Function for making basic choropleth maps using the tigris package and ggplot
+#' Function for making basic choropleth maps of shape_file using the tigris package and ggplot
 #'
 #' @param voter_file a voter file with latitude, longitude, or latitude/longitude and other geographic data.
 #' @param shape_file a shape file based on desired ecologicla unit (i.e. state, county, block, tract)
 #' @param crs the Coordinate reference system, default is crs="+proj=latlong +ellps=GRS80 +no_defs"
 #' @param title the tile of the map
 #'
-#' @export shape_file
-#' @export shape_list
+#' @export map_shape_file
 #'
 #' @return Plots of mapped ecological units desired and voter latitude and longitudes
 #'
-#' @importFrom sf
-#' @importFrom ggplot2
-#' @importFrom ggmap
+#' @import sf
+#' @import ggplot2
+#' @import ggmap
 
 map_shape_file <- function(shape_file,
                            crs = "+proj=latlong +ellps=GRS80 +no_defs",
@@ -20,10 +19,10 @@ map_shape_file <- function(shape_file,
   # Map the shapefile only and label ecological unit
   if (!is.null(shape_file)) {
     # Transform shape_file
-    shape_file <- st_transform(shape_file, crs = crs)
+    shape_file <- sf::st_transform(shape_file, crs = crs)
 
     suppressMessages(
-      sf_points <- st_centroid(shape_file)
+      sf_points <- sf::st_centroid(shape_file)
     )
 
     suppressMessages(
@@ -43,6 +42,21 @@ map_shape_file <- function(shape_file,
   return(shape_file)
 }
 
+#' Function for making basic choropleth maps of longitude and latitude points using the tigris package and ggplot
+#'
+#' @param voter_file a voter file with latitude, longitude, or latitude/longitude and other geographic data.
+#' @param shape_file a shape file based on desired ecologicla unit (i.e. state, county, block, tract)
+#' @param crs the Coordinate reference system, default is crs="+proj=latlong +ellps=GRS80 +no_defs"
+#' @param title the tile of the map
+#'
+#' @export map_shape_points
+#'
+#' @return Plots of mapped ecological units desired and voter latitude and longitudes
+#'
+#' @import sf
+#' @import ggplot2
+#' @import ggmap
+#' @import tidyr
 
 map_shape_points <- function(voter_file,
                              shape_file,
@@ -50,7 +64,7 @@ map_shape_points <- function(voter_file,
                              title = "title") {
 
   # Create lat and lon coordinates from geometry column of the full geometry voter file output
-  voter_file_geo_latlon <- extract(voter_file, geometry, into = c("lat", "lon"), "\\((.*),(.*)\\)", conv = T)
+  voter_file_geo_latlon <- tidyr::extract(voter_file, geometry, into = c("lat", "lon"), "\\((.*),(.*)\\)", conv = T)
 
   # Transform to sf
   voter_file_geo_latlon <- st_as_sf(x = voter_file_geo_latlon, coords = (25:26))
@@ -59,11 +73,11 @@ map_shape_points <- function(voter_file,
   # Establish Coordinate Reference System (CRS)
   crs <- "+proj=longlat +ellps=GRS80"
   voter_file_geo_latlon <- sf::st_transform(sf::st_set_crs(voter_file_geo_latlon, crs))
-  voter_file_geo_latlon <- st_transform(voter_file_geo_latlon, crs = st_crs(shape_file))
+  voter_file_geo_latlon <- sf::st_transform(voter_file_geo_latlon, crs = st_crs(shape_file))
   st_crs(voter_file_geo_latlon) <- st_crs(shape_file)
 
   # Merge files
-  voter_file_geo_onlyint <- st_intersection(voter_file_geo_latlon, shape_file)
+  voter_file_geo_onlyint <- sf::st_intersection(voter_file_geo_latlon, shape_file)
 
   # Distinguish between the different counties
   shape_file_polygons <- voter_file_geo_onlyint %>%
