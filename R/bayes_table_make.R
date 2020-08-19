4bc8cf51cc47dc57d06d27c95f7751ec01d8f280
Pratik Sachdeva <pratik.sachdeva23@gmail.com>
2020-08-18 18:47:45 -0700

    update bayes table make to fix error; not use any imports

--- R/bayes_table_make.R
+++ R/bayes_table_make.R
@@ -15,7 +15,7 @@
 #' Multivariate Analysis, 18(1):43, 2006.
 #' @examples
 #'
-#' # TOY DATA EXAMPLE
+#' # Toy data example
 #' \dontrun{
 #' canda <- runif(5)
 #' candb <- 1 - canda
@@ -40,7 +40,8 @@
 #'   table_names = table_names
 #' )
 #' ei_bayes_res
-#' # load package data
+#'
+#' # Example 2: Corona data
 #' data(corona)
 #' # create character vectors
 #' cands <- c(
@@ -76,52 +77,45 @@
 #' ei_bayes_res
 #' }
 #'
-#' @importFrom R.utils insert
-#' @importFrom data.table rbindlist
 #' @export bayes_table_make
-bayes_table_make <-
-  function(ei_bayes_object, cand_vector, table_names) {
+bayes_table_make <- function(ei_bayes_object, cand_vector, table_names) {
+  # Interleave the candidate names with "se" strings
+  se_cols <- rep("se", length(cand_vector))
+  rn <- c(rbind(cand_vector, se_cols))
 
-    # Used for Later Sorting/Colnames
-    seq_split <- 2:length(cand_vector)
-    rn <- c(
-      R.utils::insert(cand_vector,
-        ats = seq_split,
-        values = rep("se", length(cand_vector) - 1)
-      ),
-      "se"
-    )
-    # Summarize Bayes Object to get posterior means/devs
-    ei_bayes_object <- summary(ei_bayes_object)
-    means <- ei_bayes_object$coef # get the estimates
-    means <- data.frame(means[, "Mean"], means[, "Std. Dev."])
-    means <- t(means) # Transpose it
+  # Summarize Bayes object to get posterior means/devs
+  ei_bayes_object <- summary(ei_bayes_object)
+  # Get the estimates
+  means <- ei_bayes_object$coef
+  means <- data.frame(means[, "Mean"], means[, "Std. Dev."])
+  means <- t(means) # Transpose it
+  # Have to break apart the data to put in correct order
+  list_holder <- list()
 
-    # Have to break apart the data to put in correct order
-    list_holder <- list()
-
-    for (i in seq_len(length(cand_vector))) {
-      # use grep() to collect appropriate subsetted column names
-      subs <- grep(cand_vector[i], colnames(means), value = T)
-      # extract that data and put into list
-      subs_data <- means[, subs]
-      # need to put on same column names for rbind() later
-      colnames(subs_data) <- table_names
-      list_holder[[i]] <- subs_data
-    }
-    # put lists together into table
-    out <- data.table::rbindlist(list_holder) * 100
-    # add on column of names
-    out <- data.frame(rn, out)
-
-    # adding on total row
-    tot <- colSums(out[seq(1, nrow(out), 2), 2:ncol(out)])
-    just_data <- out[, 2:ncol(out)]
-    add <- rbind(just_data, tot)
-    add <- data.frame(seq_len(nrow(add)), add)
-    colnames(add) <- c("Candidate", table_names)
-    add[, 1] <- c(as.character(out[, 1]), "Total")
-    out <- add
-
-    return(out)
+  for (i in seq_len(length(cand_vector))) {
+    # use grep() to collect appropriate subsetted column names
+    subs <- grep(cand_vector[i], colnames(means), value = T)
+    # extract that data and put into list
+    subs_data <- means[, subs]
+    # need to put on same column names for rbind() later
+    colnames(subs_data) <- table_names
+    list_holder[[i]] <- subs_data
   }
+
+  # Put lists together into table
+  out <- do.call("rbind", list_holder) * 100
+  rownames(out) <- seq_len(nrow(out))
+  # Add on column of names
+  out <- data.frame(rn, out)
+
+  # Adding on total rows
+  tot <- colSums(out[seq(1, nrow(out), 2), 2:ncol(out)])
+  just_data <- out[, 2:ncol(out)]
+  add <- rbind(just_data, tot)
+  add <- data.frame(seq_len(nrow(add)), add)
+  colnames(add) <- c("Candidate", table_names)
+  add[, 1] <- c(as.character(out[, 1]), "Total")
+  out <- add
+
+  return(out)
+}
