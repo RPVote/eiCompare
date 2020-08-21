65003a1fc06c03caae3852226c6064623d72ef9b
Juandalyn Burke <jcburke@uw.edu>
2020-08-21 06:40:25 +0000

    Updated the map_interactive() function.

--- R/map_interactive.R
+++ R/map_interactive.R
@@ -1,11 +1,12 @@
 #' This script allows the user to plot an interactive map of the voter
 #' longitude and latitude points.
 #'
-#' @param voterfile an dataframe with a geometry column for latitude and
+#' @param voter_file an dataframe with a geometry column for latitude and
 #' longitudes created after original voter file was processed with a select
 #' geocoder.
 #' @param voter_id a unique identifier on the voter registration file.
-#' @param geometry a column with latitude and longitude points.
+#' @param geometry a column with latitude and longitude points only.
+#' This is not a column with multipolygon geometries like in a shapefile.
 #' @param lat the column (or vector) with latitude points
 #' @param lon the column (or vector) with longitude points
 #' @param first_name the column with first names of voters.
@@ -20,28 +21,28 @@
 #' @author Loren Collingwood <loren.collingwood@@ucr.edu>
 #' @author Juandalyn Burke <jcburke@@uw.edu>
 
-map_interactive <- function(voterfile,
+map_interactive <- function(voter_file,
                             voter_id = "id",
                             f_name = "firstname",
                             l_name = "lastname",
                             fips_code = "countycode") {
-  if (class(voterfile) == "data.frame") {
-    latlon_df <- tidyr::extract(voterfile,
+  if (class(voter_file) == "data.frame" & any(colnames == "geometry")) {
+    latlon_df <- tidyr::extract(voter_file,
       geometry,
       into = c("lat", "lon"), "\\((.*),(.*)\\)",
       conv = T
     )
 
-    leaflet(data = sp_latlon) %>%
+    leaflet(data = latlon_df) %>%
       addTiles() %>%
       addMarkers(~lat, ~lon,
         popup = paste(
-          "Voter ID:", registration_number, "<br>",
-          "First Name:", first_name, "<br>",
-          "Last Name:", last_name, "<br>",
-          "FIPS code:", fips_code, "<br>",
-          "Latitude:", lat, "<br>",
-          "Longitude:", lon, "<br>"
+          "Voter ID:", latlon_df[[voter_id]], "<br>",
+          "First Name:", latlon_df[[first_name]], "<br>",
+          "Last Name:", latlon_df[[last_name]], "<br>",
+          "FIPS code:", latlon_df[[fips_code]], "<br>",
+          "Latitude:", latlon_df[[lat]], "<br>",
+          "Longitude:", latlon_df[[lon]], "<br>"
         )
       )
   }
